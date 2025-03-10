383a97006b9db55c47d4eb18a04868176351a6a6
Kelvin.A <134262593+KelvinAbidha@users.noreply.github.com>
2025-03-10 05:31:36 +0300

    Name change

--- QUIZES/Quiz_one_q2.c
+++ QUIZES/Quiz_one_q2.c
@@ -11,9 +11,7 @@ void reverse(int* nums, int start, int end) {
     }
 }
 
-// Function to rotate the array to the right by k steps
 void rotate(int* nums, int numsSize, int k) {
-    k = k % numsSize; // Effective rotations
     reverse(nums, 0, numsSize - 1);
     reverse(nums, 0, k - 1);
     reverse(nums, k, numsSize - 1);
