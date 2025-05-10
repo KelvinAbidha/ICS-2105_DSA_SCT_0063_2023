05a968b2d20dac225db39db799d8d5bb2aa27bd5
Kelvin.A <134262593+KelvinAbidha@users.noreply.github.com>
2025-05-10 09:20:35 +0300

    quiz1_q2.c

---
+++ QUIZES/quiz1_q2.c
@@ -0,0 +1,36 @@
+//Question Two (2) - Rotate Array
+
+#include <stdio.h>
+
+void reverse(int* nums, int start, int end) // Function to reverse a portion of the array
+{
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+    reverse(nums, 0, numsSize - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, numsSize - 1);
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 4, 5, 6, 7};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int k = 3;
+
+    rotate(nums, numsSize, k);
+
+    printf("Rotated array: ");
+    for (int i = 0; i < numsSize; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
